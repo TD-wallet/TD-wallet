35bb2f391fa5530289e3863fcb5b57def2be3ae3
Harenabs21 <hei.harena.6@gmail.com>
2023-12-02 14:20:04 +0300

    feat: create_table_transaction.sql file

---
+++ src/main/resources/create_table_transaction.sql
@@ -0,0 +1,15 @@
+CREATE TABLE  IF NOT EXISTS transaction(
+    id int primary key ,
+    amount int not null,
+    transaction_date timestamp not null,
+    transaction_type varchar(50) not null
+);
+
+INSERT INTO transaction (id, amount, transaction_date, transaction_type)
+SELECT * FROM(
+         VALUES
+              (1, 1000.00, CURRENT_TIMESTAMP, 'Deposit'),
+              (2, 500.50, CURRENT_TIMESTAMP, 'Withdrawal'),
+              (3, 200.25, CURRENT_TIMESTAMP, 'Deposit')
+      ) AS new_transaction
+WHERE NOT EXISTS (SELECT 1 FROM transaction WHERE transaction.id = new_transaction.column1);
