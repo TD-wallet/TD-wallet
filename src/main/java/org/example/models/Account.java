ba5b57b10527320c69cd7ad7cec5a55ffd96bf7f
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-06 16:58:16 +0300

    feat: updated Account model

--- src/main/java/org/example/models/Account.java
+++ src/main/java/org/example/models/Account.java
@@ -1,19 +1,25 @@
 package org.example.models;
 
+import java.util.List;
+
 public class Account {
     private final int id;
-    private String accountNumber;
+    private String ref;
     private double balance;
+    private String type = "CASH";
+    private Currency currency;
+    private User user;
+    private List<Transaction> transactions;
 
     public Account(int id, String accountNumber, double balance) {
         this.id = id;
-        this.accountNumber = accountNumber;
+        this.ref = accountNumber;
         this.balance = balance;
     }
 
     public Account(String accountNumber) {
         this.id = 0;
-        this.accountNumber = accountNumber;
+        this.ref = accountNumber;
         this.balance = 0;
     }
 
@@ -21,12 +27,12 @@ public class Account {
         return id;
     }
 
-    public String getAccountNumber() {
-        return accountNumber;
+    public String getRef() {
+        return ref;
     }
 
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
+    public void setRef(String ref) {
+        this.ref = ref;
     }
 
     public double getBalance() {
@@ -41,8 +47,9 @@ public class Account {
     public String toString() {
         return "Account{" +
                 "id=" + id +
-                ", accountNumber='" + accountNumber + '\'' +
+                ", ref='" + ref + '\'' +
                 ", balance=" + balance +
+                ", type='" + type + '\'' +
                 '}';
     }
 }
