90eb1632758d9a0630679fb7f352c479147ad27f
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-06 21:38:24 +0300

    feat: new constructor and fields

--- src/main/java/org/example/models/User.java
+++ src/main/java/org/example/models/User.java
@@ -1,22 +1,46 @@
 package org.example.models;
 
+import java.util.List;
+
 public class User {
     private int id;
     private String username;
     private String email;
     private String password;
+    private List<Account> accounts;
+
+    public User(int id, String username, String email, String password, List<Account> accounts) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.accounts = accounts;
+    }
+
+    public User(String username, String email, String password, List<Account> accounts) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.accounts = accounts;
+    }
 
     public User(int id, String username, String email, String password) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.accounts = List.of();
     }
 
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.accounts = List.of();
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
     }
 
     public int getId() {
@@ -58,6 +82,8 @@ public class User {
                 ", username='" + username + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", accounts=" + accounts +
                 '}';
     }
+
 }
