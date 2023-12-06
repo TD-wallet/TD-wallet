1c41f767ef81677be16c5b8efe40b442ca800781
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-06 16:03:22 +0300

    feat! : changed database schema

--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -1,3 +1,9 @@
 
-CREATE DATABASE wallet;
+CREATE DATABASE wallet
+	ENCODING = 'UTF8'
+	LC_COLLATE = 'en_US.UTF-8'
+	LC_CTYPE = 'en_US.UTF-8'
+	TABLESPACE = pg_default
+	OWNER = postgres;
+
 \c wallet;
--- src/main/resources/create_table_currency.sql
+++ src/main/resources/create_table_currency.sql
@@ -1,14 +1,17 @@
-CREATE TABLE IF NOT EXISTS currency(
-    id int primary key ,
-    currency_code varchar(3) not null,
-    currency_name varchar(50) not null
+CREATE TABLE IF NOT EXISTS currency (
+	id integer NOT NULL,
+	code character varying(3) NOT NULL,
+	name character varying(70) NOT NULL,
+	symbol varchar(3),
+	CONSTRAINT currency_pkey PRIMARY KEY (id),
+	CONSTRAINT uq_code UNIQUE (code)
 );
 
-INSERT INTO currency (id,currency_code, currency_name)
+INSERT INTO currency (id, code, name, symbol)
 SELECT * FROM(
                  VALUES
-                     (1,'USD', 'US Dollar'),
-                     (2,'EUR', 'Euro'),
-                     (3,'GBP', 'British Pound')
+                     (1,'USD', 'US Dollar', '$'),
+                     (2,'EUR', 'Euro', '€'),
+                     (3,'GBP', 'British Pound', '£')
              ) AS new_currency
 WHERE NOT EXISTS (SELECT 1 FROM currency WHERE currency.id = new_currency.column1);
