f8971d02586a0aa82322ebcb05df9594910bbaad
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-07 20:53:26 +0300

    feat: added missing field in transaction

--- src/main/resources/create_table_transaction.sql
+++ src/main/resources/create_table_transaction.sql
@@ -5,17 +5,18 @@ ENUM ('DEBIT','CREDIT');
 CREATE TABLE IF NOT EXISTS transaction (
 	id integer NOT NULL,
 	amount integer NOT NULL,
+    label varchar(70),
 	date timestamp NOT NULL DEFAULT current_timestamp,
 	type "TRANSACTION_TYPE" NOT NULL DEFAULT 'DEBIT',
 	id_account integer NOT NULL,
 	CONSTRAINT transaction_pkey PRIMARY KEY (id)
 );
 
-INSERT INTO transaction (id, amount, date, type, id_account)
+INSERT INTO transaction (id, amount, label, date, type, id_account)
 SELECT * FROM(
          VALUES
-              (1, 1000.00, CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2),
-              (2, 500.50, CURRENT_TIMESTAMP, 'DEBIT'::"TRANSACTION_TYPE", 2),
-              (3, 200.25, CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2)
+              (1, 1000.00, 'Default', CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2),
+              (2, 500.50, 'Food', CURRENT_TIMESTAMP, 'DEBIT'::"TRANSACTION_TYPE", 2),
+              (3, 200.25, 'Sports', CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2)
       ) AS new_transaction
 WHERE NOT EXISTS (SELECT 1 FROM transaction WHERE transaction.id = new_transaction.column1);
