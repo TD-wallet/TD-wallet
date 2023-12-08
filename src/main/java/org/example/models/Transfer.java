f8ae4e1694783209a76e571c869956ba41493f24
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-08 16:35:37 +0300

    feat: added Transfer entity

---
+++ src/main/java/org/example/models/Transfer.java
@@ -0,0 +1,58 @@
+package org.example.models;
+
+import java.sql.Timestamp;
+
+public class Transfer {
+    private final int id;
+    private final Account debited;
+    private final Account credited;
+    private final Transaction transaction;
+    private final Timestamp date;
+
+    public Transfer(int id, Account debited, Account credited, Transaction transaction, Timestamp date) {
+        this.id = id;
+        this.debited = debited;
+        this.credited = credited;
+        this.transaction = transaction;
+        this.date = date;
+    }
+
+    public Transfer(Account debited, Account credited, Transaction transaction, Timestamp date) {
+        this.id = 0;
+        this.debited = debited;
+        this.credited = credited;
+        this.transaction = transaction;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Account getDebited() {
+        return debited;
+    }
+
+    public Account getCredited() {
+        return credited;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Transfer{" +
+                "id=" + id +
+                ", debited=" + debited +
+                ", credited=" + credited +
+                ", transaction=" + transaction +
+                ", date=" + date +
+                '}';
+    }
+}
