8db4cb736bdc751fe66c2c40cdc6e43264666f1b
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-08 09:30:36 +0300

    fix: account missing fields

--- src/main/java/org/example/repository/AccountCrudOperations.java
+++ src/main/java/org/example/repository/AccountCrudOperations.java
@@ -5,10 +5,14 @@ import org.example.utils.QueryTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccountCrudOperations implements CrudOperations<Account> {
     private final QueryTemplate qt = new QueryTemplate();
+    private final BalanceCrudOperations balanceRepo = new BalanceCrudOperations();
+    private final TransactionCrudOperations transactionRepo = new TransactionCrudOperations();
+    private final CurrencyCrudOperations currencyRepo = new CurrencyCrudOperations();
 
     @Override
     public Account findById(Integer id) {
@@ -27,18 +31,33 @@ public class AccountCrudOperations implements CrudOperations<Account> {
     }
 
     @Override
-    public List<Account> saveAll(List<Account> toSave) {
-        for (Account account : toSave) {
-            if (isNotSaved(account)) {
-                return null;
-            }
+    public List<Account> saveAll(List<Account> toSave, List<Integer> relId) {
+        ArrayList<Account> savedAccount = new ArrayList<>();
+        for (int i = 0; i < toSave.size(); i++) {
+            Account saved = save(toSave.get(i), relId.get(i));
+            if(saved == null) return  null;
+            savedAccount.add(saved);
         }
-        return toSave;
+        return savedAccount;
     }
 
     @Override
-    public Account save(Account toSave) {
-        return isNotSaved(toSave) ? null : toSave;
+    public Account save(Account toSave, int relId) {
+        if(toSave.getId() == 0) {
+            if(isSaved(toSave, relId)) {
+                return findAll().get(0);
+            }
+            return null;
+        } else if(findById(toSave.getId()) != null) {
+            return qt.executeUpdate(
+                    "UPDATE account SET name=? WHERE id=?",
+                    ps -> {
+                        ps.setString(1, toSave.getName());
+                        ps.setInt(2, toSave.getId());
+                    }
+            ) == 0 ? null : findById(toSave.getId());
+        }
+        return null;
     }
 
     @Override
@@ -54,18 +73,22 @@ public class AccountCrudOperations implements CrudOperations<Account> {
     private Account getResult(ResultSet rs) throws SQLException {
         return new Account(
                 rs.getInt("id"),
-                rs.getString("account_number"),
-                rs.getDouble("balance")
+                rs.getString("ref"),
+                balanceRepo.findByAccountId(rs.getInt("id")),
+                rs.getString("type"),
+                currencyRepo.findById(rs.getInt("id_currency")),
+                transactionRepo.findByAccountId(rs.getInt("id")),
+                rs.getString("name")
         );
     }
 
-    private boolean isNotSaved(Account toSave) {
-        return qt.executeUpdate("INSERT INTO account (id, account_number, balance) VALUES (?,?,?)",
+    private boolean isSaved(Account toSave, int relId) {
+        return qt.executeUpdate("INSERT INTO account (id, ref, id_user) VALUES (?,?,?)",
                 ps -> {
                     ps.setInt(1, this.findAll().get(0).getId() + 1);
                     ps.setString(2, toSave.getRef());
-                    ps.setDouble(3, toSave.getBalance());
+                    ps.setDouble(3, relId);
                 }
-        ) == 0;
+        ) != 0;
     }
 }
--- src/main/resources/create_table_account.sql
+++ src/main/resources/create_table_account.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS account (
 	id integer NOT NULL,
+    name varchar NOT NULL,
 	ref character varying(100) NOT NULL,
-	balance double precision NOT NULL DEFAULT 0,
 	type varchar NOT NULL,
 	id_currency integer NOT NULL,
 	id_user integer NOT NULL,
@@ -9,11 +9,11 @@ CREATE TABLE IF NOT EXISTS account (
 	CONSTRAINT account_account_number_key UNIQUE (ref)
 );
 
-INSERT INTO account (id,ref, balance, id_user, id_currency, type)
+INSERT INTO account (id, name, ref, balance, id_user, id_currency, type)
 SELECT * FROM(
                VALUES
-                   (1,'ABC123', 5000.00, 1, 3, 'BANK'),
-                   (2,'XYZ789', 7500.50, 2, 1, 'CASH'),
-                   (3,'DEF456', 3000.25, 3, 2, 'CASH')
+                   (1, 'Hello Bank', 'ABC123', 5000.00, 1, 3, 'BANK'),
+                   (2, 'Mony Wallet', 'XYZ789', 7500.50, 2, 1, 'CASH'),
+                   (3, 'Cashew', 'DEF456', 3000.25, 3, 2, 'CASH')
            ) AS new_account
 WHERE NOT EXISTS (SELECT 1 FROM account WHERE account.id = new_account.column1);
