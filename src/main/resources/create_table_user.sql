2c807e709c4cbeb556ee553e8e3a49f8881e5d6f
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-08 17:09:36 +0300

    fix: database insert

--- src/main/resources/create_table_user.sql
+++ src/main/resources/create_table_user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "user" (
-	id integer NOT NULL,
+	id serial NOT NULL,
 	username character varying(50) NOT NULL,
 	email character varying(70) NOT NULL,
 	password character varying(50) NOT NULL,
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS "user" (
 	CONSTRAINT user_email_key UNIQUE (email)
 );
 
-INSERT INTO "user" (id, username, email, password)
+INSERT INTO "user" (username, email, password)
 SELECT * FROM (
                   VALUES
-                      (1, 'john.doe', 'john.doe@email.com', 'gg1234'),
-                      (2, 'jane.smith', 'jane.smith@email.com', 'ff1234'),
-                      (3, 'david pope', 'david.james@email.com', 'edamame1313!')
+                      ('john.doe', 'john.doe@email.com', 'gg1234'),
+                      ('jane.smith', 'jane.smith@email.com', 'ff1234'),
+                      ('david pope', 'david.james@email.com', 'edamame1313!')
               ) AS new_users
-WHERE NOT EXISTS (SELECT 1 FROM "user" WHERE "user".id = new_users.column1);
+WHERE NOT EXISTS (SELECT 1 FROM "user" WHERE "user".email = new_users.column2);
