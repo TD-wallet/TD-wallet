3a473cc932dd80162762a138762f903cc77f2ae1
kirisaki-vk <kirisaki.vk@gmail.com>
2023-12-13 21:06:34 +0300

    chore: corrected typo

--- src/main/java/td/wallet/repository/TransferCrudOperations.java
+++ src/main/java/td/wallet/repository/TransferCrudOperations.java
@@ -11,7 +11,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TranferCrudOperations {
+public class TransferCrudOperations {
     private final QueryTemplate qt = new QueryTemplate();
     private final AccountCrudOperations accountRepo = new AccountCrudOperations();
 
