3434d1bca8b2a8e5fb621c5cb7a69f48b490261e
Harenabs21 <hei.harena.6@gmail.com>
2023-12-15 06:01:51 +0300

    fix: add id_category in 'create_table_transaction.sql'

--- src/main/resources/create_table_transaction.sql
+++ src/main/resources/create_table_transaction.sql
@@ -4,18 +4,19 @@ CREATE TYPE "TRANSACTION_TYPE" AS
 
 CREATE TABLE IF NOT EXISTS transaction
 (
-    id         bigserial          NOT NULL,
-    amount     integer            NOT NULL,
-    label      varchar(70)        NOT NULL DEFAULT 'Default',
-    date       timestamp          NOT NULL DEFAULT current_timestamp,
-    type       "TRANSACTION_TYPE" NOT NULL DEFAULT 'DEBIT'::"TRANSACTION_TYPE",
-    id_account integer            NOT NULL,
+    id          bigserial          NOT NULL,
+    amount      integer            NOT NULL,
+    label       varchar(70)        NOT NULL DEFAULT 'Default',
+    date        timestamp          NOT NULL DEFAULT current_timestamp,
+    type        "TRANSACTION_TYPE" NOT NULL DEFAULT 'DEBIT'::"TRANSACTION_TYPE",
+    id_account  integer            NOT NULL,
+    id_category int                NOT NULL,
     CONSTRAINT transaction_pkey PRIMARY KEY (id)
 );
 
-INSERT INTO transaction (amount, label, date, type, id_account)
+INSERT INTO transaction (amount, label, date, type, id_account, id_category)
 SELECT *
-FROM (VALUES (1000.00, 'Default', CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2),
-             (500.50, 'Food', CURRENT_TIMESTAMP, 'DEBIT'::"TRANSACTION_TYPE", 2),
-             (200.25, 'Sports', CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2)) AS new_transaction
+FROM (VALUES (1000.00, 'Default', CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2, 1),
+             (500.50, 'Food', CURRENT_TIMESTAMP, 'DEBIT'::"TRANSACTION_TYPE", 2, 3),
+             (200.25, 'Sports', CURRENT_TIMESTAMP, 'CREDIT'::"TRANSACTION_TYPE", 2, 44)) AS new_transaction
 WHERE NOT EXISTS (SELECT 1 FROM transaction WHERE transaction.id_account = new_transaction.column5);
