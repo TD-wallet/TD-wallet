a86904036145ab6afcf70093a6a7c1b7082f2c68
Harenabs21 <hei.harena.6@gmail.com>
2024-01-04 21:10:35 +0300

    feat: implement CurrencyCrudOperationsTest

---
+++ src/test/java/td/wallet/repository/CurrencyCrudOperationsTest.java
@@ -0,0 +1,61 @@
+package td.wallet.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import td.wallet.models.Currency;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CurrencyCrudOperationsTest implements CrudOperationsTest {
+    private CurrencyCrudOperations currencyCrudOperations;
+    @BeforeEach
+    public void setCurrencyCrudOperations() {
+        currencyCrudOperations = new CurrencyCrudOperations();
+    }
+
+    @Test
+    @Override
+    public void testFindById() {
+        Currency currency = currencyCrudOperations.findById(2);
+        assertNotNull(currency);
+    }
+
+    @Test
+    @Override
+    public void testFindAll() {
+        List<Currency> currencyList = currencyCrudOperations.findAll();
+        assertNotNull(currencyList);
+        assertFalse(currencyList.isEmpty());
+    }
+
+    @Test
+    @Override
+    public void testSave() {
+        Currency currency = new Currency("Ariary","MGA","Ar");
+        Currency toSave = currencyCrudOperations.save(currency);
+        assertNotNull(toSave);
+    }
+
+    @Test
+    @Override
+    public void testSaveAll() {
+        Currency currency1 = new Currency("Taka","BDT","৳");
+        Currency currency2 = new Currency("Ngultrum","BTN","Nu");
+        List<Currency> currencyList = Arrays.asList(currency1,currency2);
+        List<Currency> toSave = currencyCrudOperations.saveAll(currencyList);
+        assertNotNull(toSave);
+        assertEquals(currencyList.size(),toSave.size());
+    }
+
+    @Test
+    @Override
+    public void testDelete() {
+        Currency currency = currencyCrudOperations.findById(5);
+        Currency toDelete = currencyCrudOperations.delete(currency);
+        assertNotNull(toDelete);
+        assertEquals(currency.getId(),toDelete.getId());
+    }
+}
