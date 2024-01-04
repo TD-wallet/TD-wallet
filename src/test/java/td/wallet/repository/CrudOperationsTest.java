4553360fa795687d83283993e2a6eef162fe7143
Harenabs21 <hei.harena.6@gmail.com>
2024-01-04 16:58:29 +0300

    feat: implement CrudOperationsTest

---
+++ src/test/java/td/wallet/repository/CrudOperationsTest.java
@@ -0,0 +1,16 @@
+package td.wallet.repository;
+
+
+public interface CrudOperationsTest {
+
+    void testFindById();
+
+    void testFindAll();
+
+    void testSave();
+
+    void testSaveAll();
+
+    void testDelete();
+
+}
