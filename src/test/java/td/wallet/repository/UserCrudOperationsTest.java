4e07e34d3a715f8928ddd828ac3f9326de1234a5
Harenabs21 <hei.harena.6@gmail.com>
2024-01-04 14:27:50 +0300

    feat: implement UserCrudOperationsTest

---
+++ src/test/java/td/wallet/repository/UserCrudOperationsTest.java
@@ -0,0 +1,52 @@
+package td.wallet.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import td.wallet.models.User;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class UserCrudOperationsTest {
+    private  UserCrudOperations userCrudOperations;
+
+    @BeforeEach
+    public void setUserCrudOperations(){
+         userCrudOperations = new UserCrudOperations();
+    }
+
+    @Test
+    public void testFindById(){
+        User existinguser = userCrudOperations.findById(1);
+        // Assert
+        assertNotNull(existinguser);
+
+    }
+    @Test
+    public void testFindAll(){
+        List<User> userList = userCrudOperations.findAll();
+        assertNotNull(userList);
+        assertFalse(userList.isEmpty());
+    }
+
+    @Test
+    public void testSave(){
+        // Arrange
+        User userToSave = new User(0, "newUser", "newUser@email.com", "newPassword");
+
+        // Act
+        User savedUser = userCrudOperations.save(userToSave);
+        assertNotNull(savedUser);
+        assertNotEquals(0, savedUser.getId());
+    }
+
+    @Test
+    public  void testDelete(){
+        User userToDelete = userCrudOperations.findById(4);
+        User userDeleted = userCrudOperations.delete(userToDelete);
+
+        assertNotNull(userDeleted);
+        assertEquals(userToDelete.getId(),userDeleted.getId());
+    }
+
+}
