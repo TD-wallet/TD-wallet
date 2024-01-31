e97b5ab637341b8d9252ccb484e60d40e6ec3fba
Harenabs21 <hei.harena.6@gmail.com>
2024-01-31 10:23:12 +0300

    feat: create BalanceRequest in dto

---
+++ src/main/java/td/wallet/dto/BalanceRequest.java
@@ -0,0 +1,24 @@
+package td.wallet.dto;
+
+import td.wallet.models.Balance;
+
+import java.util.List;
+
+public class BalanceRequest {
+    private List<Balance> balances;
+    private List<Integer> relId;
+
+    public BalanceRequest(List<Balance> balances, List<Integer> relId) {
+        this.balances = balances;
+        this.relId = relId;
+    }
+
+    public List<Balance> getBalances() {
+        return balances;
+    }
+
+    public List<Integer> getRelId() {
+        return relId;
+    }
+
+}
